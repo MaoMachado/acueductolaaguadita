94dd4ad00be8a81fcac1f408ba038c00dc224a42
MaoMachado <114309283+MaoMachado@users.noreply.github.com>
2026-06-16 23:50:44 -0500

    update: mejora visual login

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,64 +1,130 @@
 <script setup>
-import { ref, defineEmits } from 'vue';
+import { ref, defineEmits } from "vue";
+import userIcon from "../assets/images/user-svg.png";
+import passIcon from "../assets/images/pass-svg.png";
 
 const API = import.meta.env.VITE_API_URL;
 
-const usuario = ref('');
-const password = ref('');
-const error = ref('');
-const emit = defineEmits(['login-exitoso']);
+const usuario = ref("");
+const password = ref("");
+const error = ref("");
+const cargando = ref(false);
+const emit = defineEmits(["login-exitoso"]);
 
 async function login() {
+  cargando.value = true;
+  error.value = "";
+
+  if (!usuario.value.trim().toLocaleLowerCase() || !password.value.trim()) {
+    error.value = "⚠️ Todos los campos son obligatorios";
+    setTimeout(() => (error.value = ""), 5000);
+    cargando.value = false;
+    return;
+  }
+
   try {
     const res = await fetch(`${API}/login`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         username: usuario.value,
-        password: password.value
-      })
-    })
+        password: password.value,
+      }),
+    });
 
     if (!res.ok) {
       const data = await res.json();
-      throw new Error(data.error || 'Error de autenticación');
+      throw new Error(data.error || "Error de autenticación");
     }
 
-    localStorage.setItem('logueado', 'true');
-    emit('login-exitoso')
+    localStorage.setItem("logueado", "true");
+    emit("login-exitoso");
   } catch (error) {
     error.value = error.message;
+  } finally {
+    cargando.value = false;
   }
 }
 </script>
 
 <template>
-  <section class="flex flex-col gap-4 max-w-xs p-6 bg-white rounded-2xl shadow">
-    <h2 class="text-2xl font-bold text-center">Inicio De Sesión</h2>
+  <section
+    class="max-w-sm bg-(--gris-suave-60) p-8 border-2 border-(--beige-oscuro-60) rounded-2xl shadow shadow-(--verde-oscuro-60)"
+  >
+    <div>
+      <h2 class="text-3xl font-semibold text-center text-(--verde-claro) mb-4">
+        Inicio De Sesión
+      </h2>
 
-    <form @submit.prevent="login">
-      <input type=" text" v-model="usuario" placeholder="Usuario" class="input mb-4">
-      <input type="password" v-model="password" placeholder="Contraseña" class="input mb-4">
+      <form @submit.prevent="login" class="flex flex-col gap-4">
+        <div class="flex gap-2 items-center">
+          <label for="usuario" class="w-8 h-8">
+            <img
+              :src="userIcon"
+              alt="Usuario"
+              class="w-full h-full object-cover"
+            />
+          </label>
+          <input
+            type=" text"
+            v-model="usuario"
+            id="usuario"
+            class="text-xl bg-(--blanco) border-2 border-(--beige-oscuro-60) rounded-md px-4 py-1 focus:outline-none focus:border-(--verde-claro) focus:ring-1 focus:ring-(--verde-claro-60) transition-all duration-300"
+          />
+        </div>
 
-      <button type="submit"
-        class="w-full mt-4 bg-green-700 hover:bg-green-800 text-white font-bold py-2 rounded">Iniciar Sesión</button>
+        <div class="flex gap-2 items-center">
+          <label for="password" class="w-8 h-8">
+            <img
+              :src="passIcon"
+              alt="Contraseña"
+              class="w-full h-full object-cover"
+            />
+          </label>
+          <input
+            type="password"
+            v-model="password"
+            id="password"
+            class="text-xl bg-(--blanco) border-2 border-(--beige-oscuro-60) rounded-md px-4 py-1 focus:outline-none focus:border-(--verde-claro) focus:ring-1 focus:ring-(--verde-claro-60) transition-all duration-300"
+          />
+        </div>
 
-      <p v-if="error" class="text-red-600 mt-2 text-sm">{{ error }}</p>
-    </form>
+        <button
+          type="submit"
+          :disabled="cargando"
+          class="w-full mt-4 disabled:opacity-50 disabled:cursor-not-allowed bg-(--verde-claro-40) hover:bg-(--verde-claro) font-bold py-2 rounded cursor-pointer"
+        >
+          <span v-if="cargando" class="flex items-center gap-2 justify-center">
+            <svg class="animate-spin h-4 w-4" viewBox="0 0 24 24">
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+                fill="none"
+              />
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              />
+            </svg>
+            <span class="text-center">Iniciando Sesión...</span>
+          </span>
+          <span v-else class="flex items-center gap-2 justify-center"
+            >Iniciar Sesión</span
+          >
+        </button>
+
+        <p
+          v-if="error"
+          class="py-2 px-4 rounded-lg bg-red-50 border border-red-200 text-red-700 mt-2 text-xl text-center"
+        >
+          {{ error }}
+        </p>
+      </form>
+    </div>
   </section>
 </template>
-
-<style scoped>
-.input {
-  inline-size: 100%;
-  padding: 0.5rem;
-  border-radius: 1rem;
-  border: 2px solid var(--beige-oscuro);
-  font-family: var(--fuente-parrafo);
-  padding-inline-start: 1rem;
-}
-
-button {
-  font-family: var(--fuente-titulo);
-}
-</style>
--- frontend/src/views/Archivos.vue
+++ frontend/src/views/Archivos.vue
@@ -1,4 +1,4 @@
-<script setup>
+ <script setup>
 
 import { ref, onMounted } from 'vue';
 import SubirArchivoDialog from '../components/SubirArchivoDialog.vue';
